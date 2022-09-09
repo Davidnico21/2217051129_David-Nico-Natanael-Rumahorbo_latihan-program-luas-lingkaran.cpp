524f34dd5454de70c24b1198d0cbc1b2a9d87fe6
David nico <112567725+Davidnico21@users.noreply.github.com>
2022-09-09 15:20:28 +0700

    Add files via upload

---
+++ 2217051129_David Nico Natanael Rumahorbo_latihan program luas lingkaran.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+	int main(){	
+	float luas,r,phi = 3.14;
+	
+	cout<<"hitung luas lingkaran \n";
+	
+	cout<<"masukkan jari jari : \n";
+	cin>>r;
+	
+	luas=phi*r*r;
+	
+	cout<<"luas lingkaran dari jari-jari tersebut adalah :"<<luas;
+	
+	
+	return 0;
+}
